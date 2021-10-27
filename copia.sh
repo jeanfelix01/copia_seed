3c10fa1ecc095d5b55d17f3abe00c7f147c79464
jeanfelix01 <jeanfelix_@hotmail.com>
2021-10-27 14:49:06 -0300

    criacao

---
+++ copia.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+IFS='
+'
+
+##Script para automatizar a movimentação de torrents da seedbox local para a seedbox  no TD
+#versao 0.1
+
+arquivos='/mnt/torrent-seed/torrents-netfelix/arquivos-torrents/'
+antigos='/mnt/torrent-seed/torrents-netfelix/torrents-antigos/'
+
+
+echo Movendo .torrent 'ARQUIVOS -> ANTIGOS'
+sleep 5
+find $arquivos  -name "*.torrent" | xargs -I '{}' -exec mv {} $antigos -v
+echo
+echo Movendo .torrets 'LOCAL -> ARQUIVOS'
+echo
+sleep 5
+find "/mnt/torrent/" -name "*.torrent" | xargs -I '{}' -exec mv {} $arquivos -v
+echo
+sleep 5
+echo Iniciando copia dos TORRENTS para o TD
+echo
+sleep 5
+rclone copy "/mnt/downloads/qbittorrent/" "drivetorrents:torrents-netfelix/downloads/" -P
+echo
+sleep 5
+echo Apagando Torrents da maquina local
+echo
+sleep 5
+rm -r -f -v /mnt/downloads/qbittorrent/*
+echo
+sleep 5
+echo Script finalizado ....
